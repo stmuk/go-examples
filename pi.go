c7cf53879fc311567b82e80c4099a9b1d7cd048e
Sue Spence <virtuallysue@gmail.com>
2015-09-21 19:39:20 +0100

    Add var declarations. Make it a bit more idiomatic (i think).

--- pi.go
+++ pi.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-func main () {
+func main() {
 
-    n = 100000;
-    delta = 1.0 / n;
-    sum = 0.0;
-    i = 1;
-    for ( i <= n) {
-          x = ( i - 0.5 ) * delta;
-            sum += 1.0 / ( 1.0 + x * x );
-              i += 1;
-          }
-          pi = 4.0 * delta * sum;
-          fmt.Println(pi);
+	var n = 100000
+	var delta float64 = 1.0 / float64(n)
+	var sum float64 = 0.0
+	var x float64
+
+	for i := 1; i <= n; i++ {
+		x = (float64(i) - 0.5) * delta
+		sum += 1.0 / (1.0 + x*x)
+	}
+	var pi float64 = 4.0 * delta * sum
+	fmt.Println("Pi is approximately", pi, "after", n, "iterations.")
 }
-
